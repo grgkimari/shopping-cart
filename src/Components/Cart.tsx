c96e994724930c8002c6e0d20d4c7491a0a86df6
George Kimari <grgkimari@gmail.com>
2023-04-16 23:01:21 +0300

    Added Shopping cart icon

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -1,7 +1,15 @@
+import { ShoppingCart } from "@mui/icons-material"
+import { Stack } from "@mui/system"
+
 const Cart = () => {
     return(
         <div>
-            <Navigator />
+            <Stack spacing={2} sx={{
+                bgcolor : "grey",
+                width : "100%"
+            }}><Stack sx={{
+                bgcolor : "lightblue"
+            }}><ShoppingCart /></Stack></Stack>
         </div>
     )
 }
